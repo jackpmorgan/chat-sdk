2d3274b4760dc70cbde05b9ba8e95c6b55faf0d3
Jack Morgan <66273764+jackpmorgan@users.noreply.github.com>
2025-07-15 08:52:07 -0400

    Add Windows 95 and Futuristic themes

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,5 +4,12 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { ThemeProviderProps } from 'next-themes/dist/types';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider
+      themes={['light', 'dark', 'win95', 'futuristic']}
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
